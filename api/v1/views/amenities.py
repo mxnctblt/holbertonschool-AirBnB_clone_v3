a4b611f989ce947407e72da03c518cc5e90ed28f
mxnctblt <thibaultmaxence44@gmail.com>
2023-04-14 06:18:08 -0700

    9. Amenity

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Create a new view for Amenity objects that handles
+all default RESTFul API actions """
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from flasgger import swag_from
+from models import storage
+from models.amenity import Amenity
+
+
+@app_views.route("/amenities", methods=["GET"], strict_slashes=False)
+def amenity_get_all():
+    """ Retrieves the list of all Amenity objects """
+    return jsonify([a.to_dict() for a in storage.all("Amenity").values()])
+
+@app_views.route("/amenities/<amenity_id>", methods=["GET"],
+                 strict_slashes=False)
+def amenity_get(amenity_id):
+    """ Retrieves a Amenity object """
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+@app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def amenity_delete(amenity_id):
+    """ Deletes a Amenity object """
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({})
+
+@app_views.route("/amenities", methods=["POST"],
+                 strict_slashes=False)
+def amenity_create(amenity_id):
+    """ Creates a Amenity """
+    amenity = storage.get(Amenity, amenity_id)
+    props = request.get_json()
+    if type(props) != dict:
+        abort(400, description="Not a JSON")
+    if not props.get("name"):
+        abort(400, description="Missing name")
+    new_amenity = Amenity(**props)
+    new_amenity.amenity_id = amenity_id
+    new_amenity.save()
+    return jsonify(new_amenity.to_dict()), 201
+
+@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
+                 strict_slashes=False)
+def amenity_update(amenity_id):
+    """ Updates a Amenity object """
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    props = request.get_json()
+    if type(props) != dict:
+        abort(400, description="Not a JSON")
+    for key, value in props.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(amenity, key, value)
+    storage.save()
+    return jsonify(city.to_dict()), 200
